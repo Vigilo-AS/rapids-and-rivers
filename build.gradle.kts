23c1e892800475a7c9ba40cc73d99575e0ad6317
Frode Sjovatsen <frode.sjovatsen@gmail.com>
2024-09-25 14:25:20 +0200

    Added publish config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     kotlin("jvm") version "2.0.20"
     `java-library`
+    java
+    `maven-publish`
+
 }
 
 group = "no.vigilo"
@@ -37,4 +40,43 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+val githubUser: String? by project
+val githubPassword: String? by project
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/vigilo-as/rapids-and-rivers")
+            credentials {
+                username = githubUser
+                password = githubPassword
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+            pom {
+                name.set("rapids-rivers")
+                description.set("Rapids and Rivers")
+                url.set("https://github.com/vigilo-as/rapids-and-rivers")
+
+//                licenses {
+//                    license {
+//                        name.set("MIT License")
+//                        url.set("https://opensource.org/licenses/MIT")
+//                    }
+//                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/vigilo-as/rapids-and-rivers.git")
+                    developerConnection.set("scm:git:https://github.com/vigilo-as/rapids-and-rivers.git")
+                    url.set("https://github.com/vigilo-as/rapids-and-rivers")
+                }
+            }
+            from(components["java"])
+        }
+    }
 }
