1d0cff398c06aae06e717d7a7914be17ef58397d
Frode Sjovatsen <frode.sjovatsen@gmail.com>
2024-10-11 21:56:21 +0200

    We're now able to create a SpringRapidApplication with custom rapid connection

--- src/main/kotlin/no/vigilo/SpringRapidApplication.kt
+++ src/main/kotlin/no/vigilo/SpringRapidApplication.kt
@@ -8,17 +8,37 @@ import no.vigilo.rapids_and_rivers_api.RapidsConnection
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 
-class SpringRapidApplication(private val localKafka: Boolean, applicationEventsWithKeys: Boolean) : Runnable {
+class SpringRapidApplication(
+    private val localKafka: Boolean,
+    applicationEventsWithKeys: Boolean,
+    private val rapid: RapidsConnection = RapidApplication.create(
+        System.getenv(),
+        ConsumerProducerFactory(if (localKafka) LocalConfig.default else AivenConfig.default),
+        applicationEventsWithKeys
+    )
+) : Runnable {
 
-    private val rapid = RapidApplication.create(System.getenv(), ConsumerProducerFactory(config()), applicationEventsWithKeys)
+    //private val rapid = RapidApplication.create(System.getenv(), ConsumerProducerFactory(config()), applicationEventsWithKeys)
 
     companion object {
         /**
          * Create a new SpringRapidApplication instance.
          * @param localKafka Boolean whether to use a local Kafka instance. Defaults to false.
          * @param applicationEventsWithKeys Boolean whether to publish application events with a key. The key is a random UUID.
+         * @return SpringRapidApplication
          */
-        fun create(localKafka: Boolean, applicationEventsWithKeys: Boolean) = SpringRapidApplication(localKafka, applicationEventsWithKeys)
+        fun create(localKafka: Boolean, applicationEventsWithKeys: Boolean) =
+            SpringRapidApplication(localKafka, applicationEventsWithKeys)
+
+        /**
+         * Create a new SpringRapidApplication instance with rapid connection.
+         * @param localKafka Boolean whether to use a local Kafka instance. Defaults to false.
+         * @param applicationEventsWithKeys Boolean whether to publish application events with a key. The key is a random UUID.
+         * @param rapid RapidsConnection the rapid connection to use.
+         * @return SpringRapidApplication
+         */
+        fun create(localKafka: Boolean, applicationEventsWithKeys: Boolean, rapid: RapidsConnection) =
+            SpringRapidApplication(localKafka, applicationEventsWithKeys, rapid)
     }
 
     @EventListener(ApplicationReadyEvent::class)
@@ -45,5 +65,4 @@ class SpringRapidApplication(private val localKafka: Boolean, applicationEventsW
         rapid.start()
     }
 
-    private fun config() = if (localKafka) LocalConfig.default else AivenConfig.default
 }
